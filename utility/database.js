4e99d21b7b1a8d8fa9665c1733fb6e286869d526
Adam Kerik <ajkerik@gmail.com>
2020-05-13 17:12:21 -0400

    con#all > con#get

--- utility/database.js
+++ utility/database.js
@@ -35,7 +35,7 @@ class Database {
 
         let rows;
         try {
-          rows = stmt.get(args);
+          rows = stmt.all(args);
           resolve(rows);
         } catch(e) {
           reject(e);
@@ -63,31 +63,5 @@ class Database {
     });
   }
 }
-
-// class Database {
-//   constructor(config) {
-//     if(settings.databaseType && settings.databaseType === 'SQLITE')
-//       this.connection
-//     // this.connection = mysql.createConnection(config);
-//   }
-//   query(sql, args) {
-//     return new Promise((resolve, reject) => {
-//       this.connection.query(sql, args, (err, rows) => {
-//         if (err)
-//           return reject(err);
-//         resolve(rows);
-//       });
-//     });
-//   }
-//   close() {
-//     return new Promise((resolve, reject) => {
-//       this.connection.end(err => {
-//         if (err)
-//           return reject(err);
-//         resolve();
-//       });
-//     });
-//   }
-// }
 
 module.exports = Database;
